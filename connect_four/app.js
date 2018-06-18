be7112b9cd8cb51e60c9a19512d51a2ca012eede
Kellie Dambroso <kelliedambroso@Kellies-MacBook-Pro.local>
2018-06-18 11:26:50 -0500

    intitial commit

--- connect_four/app.js
+++ connect_four/app.js
@@ -1,26 +1,35 @@
 console.log('Are you ready to connect?')
 
-
+$(()=> {
 
 
 //Create Board
-class ConnectFour {
-  constuctor (rows, columns, selector) {
-    this.rows = 6;
-    this.columns = 7;
-    this.selector = selector;
-    this.createGrid();
-  }
-  createGrid(){
-    const $board = $(this.selector);
-    console.log($board);
-    for (let row = 0; row < this.rows; row++) {
-      const $row = $('<div>').addClass('row');
-      $board.append($row);
-    }
-    console.log($board);
-  }
+
+for (let row=0; row< 6 ;row++){
+  const $divR = $('<div>').addClass('row')
+  $('#container').append($divR)
+  $divR.on('mouseenter','.column', ()=>{
+   console.log('here')
+ })
 }
+for (let column=0; column < 7 ;column++){
+    const $divC = $('<div>').addClass('column')
+    .attr('data-column', column)
+    $('.row').append($divC)
+  }
+
+// Setup Event Listeners
 
 
-const connect4 = new ConnectFour('#connect4')
+
+
+
+
+
+
+
+
+
+
+
+})
