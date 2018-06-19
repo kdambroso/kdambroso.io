ba845779ee3f8d4d8ce8eb62883ff3cf3eccb68e
Kellie Dambroso <kelliedambroso@Kellies-MacBook-Pro.local>
2018-06-19 15:18:36 -0500

    alternate player

--- connect_four/app.js
+++ connect_four/app.js
@@ -10,6 +10,7 @@ class Connect4 {
     this.selector = selector;
     this.createGrid();
     this.eventListeners();
+
   }
   createGrid() {
   //creates 6x7 grid (6 rows, 7 columns) gameboard with divs
@@ -28,26 +29,86 @@ class Connect4 {
     }
   } //end gameboard
 
+
+//function to check row below event target
+// const $('lastEmpty') = (columnNum, rowNum) => {
+//   if(columnNum < 3){
+//     console.log('less than 3')
+//   } else {
+//     console.log('greater than 3')
+//   }
+// }
+
+
+
 // Setup Event Listeners
   eventListeners(){
       const $board = $(this.selector);
 
 //Finds column and row index
       $board.on('click', '.column.empty', ()=> {
-        // console.log('here', event.target);
-        if ($(event.target).hasClass('empty')){
-          console.log('open spot')
+        console.log('here', event.target);
+        changePlayer()
+        if (player1 === true) {
           $(event.target).removeClass('empty').addClass('player1')
-          console.log('here', event.target)
+          let columnNum= $(event.target).attr('data-column')
+          let rowNum= $(event.target).attr('data-row')
+          console.log(columnNum, rowNum)
+
+
+            if(columnNum < 3){
+              console.log('less than 3')
+            } else {
+              console.log('greater than 3')
+            }
+
+
+
+          //run function to check row below event target
+
+          // lastEmpty(columnNum, rowNum)
+        } else if (player1 === false){
+          $(event.target).removeClass('empty').addClass('player2')
+          //run function to check row below event target
+          let columnNum= $(event.target).attr('data-column') // keep data column
+          let rowNum= $(event.target).attr('data-row') // to check below +1
+          if(columnNum < 3){
+            console.log('less than 3')
+          } else {
+            console.log('greater than 3')
+          }
         }
 
-      })
-    }
-  }
 
-// Game Play:
+
+
+          // $(event.target).removeClass('empty').addClass('player1')
+          // console.log('here', event.target)
+        // }
+})
+}
+}
+// Game Play Logic:
 
 //There are two players that alternate turns.
+//alternate clicks between player1 and player2
+
+  let player1 = null;
+  const changePlayer = () =>{
+    if(player1) {
+        $(event.target).hasClass('player1');
+        player1  = false;
+      }  else {
+        player1  = true;
+      }
+    }
+
+    if (player1 === true) {
+      p1()
+    } else if (player1 === false){
+      p2()
+    }
+
 
 
 //During each turn, a player places a token in one of the columns. The token slides to the "bottom" of the column. So the first move of the game would have the token at the bottom of the column in which it is placed (row 6); if the other player then plays in the same column, that player's token would be above the other player's token (so the new token is at row 5), etc.
