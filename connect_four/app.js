39b2e06ff9162093a4a18684c8984426c0b96e81
Kellie Dambroso <kelliedambroso@Kellies-MacBook-Pro.local>
2018-06-20 15:42:51 -0500

    checks for player 1 winner

--- connect_four/app.js
+++ connect_four/app.js
@@ -35,14 +35,17 @@ class Connect4 {
 // Setup Event Listeners
 gameLogic(){
   const $board = $(this.selector);
+//During each turn, a player places a token in one of the columns. The token slides to the "bottom" of the column.
 //function to check row below event target for lastEmpty Cell to drop token into
   const lastEmpty = (columnNum) => {
+    //finds all cells in the column
     const cells = $(`.column[data-column='${columnNum}']`);
-    console.log(cells)
+    // console.log(cells)
+    //checks each one for the class empty and stops when it finds on that isn't
     for (let i = cells.length-1; i>=0; i-- ){
       const $cell = $(cells[i]);
       if ($cell.hasClass('empty')) {
-        console.log($cell)
+        // console.log($cell)
         return $cell;
       }
     }
@@ -58,16 +61,73 @@ gameLogic(){
     changePlayer()
 
     if (player1 === true) {
+      //if gameOver is not true
 
   //run function to check row below event target
       let columnNum= $(event.target).attr('data-column')
       let rowNum= $(event.target).attr('data-row')
       const $lastEmpty = lastEmpty(columnNum)
-      console.log(columnNum, rowNum)
-      // lastEmpty(columnNum)
-
-
       $lastEmpty.removeClass('empty').addClass('player1')
+      // console.log($("[data-column= '0'][data-row='0']"));
+
+    //vertical check
+      for (let x=0; x<=4; x++){
+        for (let y=0; y<=5; y++) {
+          let a= x+1
+          let b= x+2
+          let c= x+3
+          if ($("[data-column= "+y+"][data-row= "+x+"]").hasClass('player1') &&
+          $("[data-column= "+y+"][data-row= "+a+"]").hasClass('player1') && $("[data-column= "+y+"][data-row= "+b+"]").hasClass('player1') && $("[data-column= "+y+"][data-row= "+c+"]").hasClass('player1') ){
+            console.log('WINNER');
+          }
+        }
+      }
+
+      //horizontal check
+        for (let x=0; x<=6; x++){
+          for (let y=0; y<=3; y++) {
+            let a= y+1
+            let b= y+2
+            let c= y+3
+            if ($("[data-column= "+y+"][data-row= "+x+"]").hasClass('player1') &&
+            $("[data-column= "+a+"][data-row= "+x+"]").hasClass('player1') && $("[data-column= "+b+"][data-row= "+x+"]").hasClass('player1') && $("[data-column= "+c+"][data-row= "+x+"]").hasClass('player1') ){
+              console.log('WINNER');
+            }
+          }
+        }
+        //diagonal up check
+          for (let x=0; x<=4; x++){
+            for (let y=3; y<=6; y++) {
+              let a= y-1
+              let b= y-2
+              let c= y-3
+              let d= x+1
+              let e= x+2
+              let f= x+3
+              if ($("[data-column= "+y+"][data-row= "+x+"]").hasClass('player1') &&
+              $("[data-column= "+a+"][data-row= "+d+"]").hasClass('player1') && $("[data-column= "+b+"][data-row= "+e+"]").hasClass('player1') && $("[data-column= "+c+"][data-row= "+f+"]").hasClass('player1') ){
+                console.log('WINNER');
+              }
+            }
+          }
+          //diagonal down check
+            for (let x=0; x<=4; x++){
+              for (let y=0; y<=3; y++) {
+                let a= y+1
+                let b= y+2
+                let c= y+3
+                let d= x+1
+                let e= x+2
+                let f= x+3
+                if ($("[data-column= "+y+"][data-row= "+x+"]").hasClass('player1') &&
+                $("[data-column= "+a+"][data-row= "+d+"]").hasClass('player1') && $("[data-column= "+b+"][data-row= "+e+"]").hasClass('player1') && $("[data-column= "+c+"][data-row= "+f+"]").hasClass('player1') ){
+                  console.log('WINNER');
+                }
+              }
+            }
+
+
+
 
     } else if (player1 === false){
 
@@ -75,8 +135,8 @@ gameLogic(){
       let columnNum= $(event.target).attr('data-column') // keep data column
       let rowNum= $(event.target).attr('data-row') // to check below +1
       const $lastEmpty = lastEmpty(columnNum)
-      console.log(columnNum, rowNum)
-      // lastEmpty(columnNum, rowNum)
+
+
       $lastEmpty.removeClass('empty').addClass('player2')
     }
   })
@@ -105,16 +165,11 @@ gameLogic(){
 
 
 
-//During each turn, a player places a token in one of the columns. The token slides to the "bottom" of the column. So the first move of the game would have the token at the bottom of the column in which it is placed (row 6); if the other player then plays in the same column, that player's token would be above the other player's token (so the new token is at row 5), etc.
-//
-
-
 // Check for Winner:
 
 
-// restart () {
-//   this.createGrid();
-// }
+
+
 //A player wins when four tokens, from the same player, are arranged either horizontally (all on the same row),
 
 
@@ -123,8 +178,12 @@ gameLogic(){
 
 //or diagonally (each token one column left or right, one row up or down, from another token), consecutively (no breaks).Once a player reaches a winning condition, the game should stop, and the program should show the message "Player won!"
 
-// If the player chooses a column that is invalid or full (the column has no more empty rows), then the program should ask for a new column choice:
 
+
+
+// restart () {
+//   this.createGrid();
+// }
 
 
 const connectFour = new Connect4('#connect4')
